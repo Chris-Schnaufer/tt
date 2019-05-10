32f2252ccd58235033ee4576c5a81b91795e57f8
Chris Schnaufer <schnaufer@email.arizona.edu>
2019-05-10 12:11:22 -0700

    Fixed error

--- test/wait_for_clowder.sh
+++ test/wait_for_clowder.sh
@@ -6,6 +6,7 @@ set -ev
 CLOWDER_NAME=$1
 if [[ -z "${CLOWDER_NAME}" ]]; then
     CLOWDER_NAME=`docker ps | grep "clowder_1" | sed -r 's/.*(\b.+_clowder_1).*/\1/'`
+fi
 SLEEP_SECONDS=10
 
 if [[ -z "$CLOWDER_NAME" ]]; then
@@ -23,6 +24,7 @@ do
     echo "Sleep before checking container: ${COUNT}"
     ((COUNT=COUNT+1))
     sleep "${SLEEP_SECONDS}"
+    echo 'docker logs "${CLOWDER_NAME}" 2>&1 | grep "Listening for HTTP on /0.0.0.0:9000"'
     docker logs "${CLOWDER_NAME}" 2>&1 > clog.txt
     RES=`grep "Listening for HTTP on /0.0.0.0:9000" clog.txt || echo ""`
     if [[ -n "${RES}" ]]; then break; fi
