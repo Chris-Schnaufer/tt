3caddde9d57567fe9ceb27445ea49e92afcec807
Chris Schnaufer <schnaufer@email.arizona.edu>
2019-05-15 12:27:09 -0700

    Test

--- test/start_extract.py
+++ test/start_extract.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
-
+  
 """Starts the extract process
 """
 
 import sys
+import json
 import requests
 
 data_load = sys.argv[1]
@@ -12,7 +13,11 @@ uri = sys.argv[2]
 print("Data: >" + data_load + "<")
 print("URL: " + uri)
 
+json_data = json.loads(str(data_load))
 headers = {"accept": "application/json", "Content-Type":"application/json"}
 
-res = requests.post(uri, headers=headers, data=data_load)
+print("JSON data: "+str(json_data))
+print("headers: " + str(headers))
+
+res = requests.post(uri, headers=headers, data=json.dumps(json_data))
 res.raise_for_status()
