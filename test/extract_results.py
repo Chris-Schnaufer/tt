e73fe6d1d71fc9a32035e1af0a9495be1fc531eb
Chris Schnaufer <schnaufer@email.arizona.edu>
2019-05-16 14:59:59 -0700

    Changing how things work

--- test/extract_results.py
+++ test/extract_results.py
@@ -13,10 +13,10 @@ clowder_uri = os.getenv("CLOWDER_HOST_URI", "http://localhost:9000")
 API_BASE = "%s/api" % (clowder_uri)
 
 # Get the name of the test dataset
-test_ds_name = "test_dataset"
+fetch_ds_name = None
 argc = len(sys.argv)
 if argc > 1:
-    test_ds_name = sys.argv[1]
+    fetch_ds_name = sys.argv[1]
 
 # Get all the dataset names
 key = os.getenv("API_KEY")
@@ -24,20 +24,21 @@ KEY_PARAM = "key=%s" % (key)
 headers = {"accept": "application/json"}
 
 url = "%s/datasets?%s" % (API_BASE, KEY_PARAM)
-res = requests.post(url, headers=headers)
+res = requests.get(url, headers=headers)
 res.raise_for_status()
 
 # Create a storage locations for datasets
 destdir = "./datasets"
-os.makedirs(destdir)
+if not os.path.isdir(destdir):
+    os.makedirs(destdir)
 
 # Get all the datasets
 return_ds = {}
 datasets = res.json()
 for ds in datasets:
-    if 'name' in ds and 'id' in ds and ds['name'] != test_ds_name:
+    if 'name' in ds and 'id' in ds and (fetch_ds_name is None or ds['name'] == fetch_ds_name):
         url = "%s/datasets/%s/files?%s" % (API_BASE, ds['id'], KEY_PARAM)
-        res = requests.post(url, headers=headers)
+        res = requests.get(url, headers=headers)
         res.raise_for_status()
 
         # Download and store each file in the dataset under the dataset name
@@ -51,9 +52,9 @@ for ds in datasets:
             filepath = os.path.join(destdir, ds['name'])
             if not os.path.isdir(filepath):
                 os.makedirs(filepath)
-            dest = os.path.join(filepath, fn['name'])
+            dest = os.path.join(filepath, fn['filename'])
             try:
-                with os.fdopen(dest, "w") as out_file:
+                with open(dest, "w") as out_file:
                     for chunk in res.iter_content(chunk_size=10*1024):
                         out_file.write(dest)
             except:
